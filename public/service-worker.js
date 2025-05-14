26a5dabccbb18cbc4b31ebc9cb0eb5585bd50584
smaxxxyy <smaxxxyy@gmail.com>
2025-05-14 10:51:08 +0500

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,7 @@
+self.addEventListener('push', (event) => {
+  const data = event.data.json();
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: '/favicon.ico', // Replace with your app's icon path
+  });
+});
